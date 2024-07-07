c22236e40e4b0b5535399f0e8eadcf8b9ea009b0
Денис <tolmachevdenistolmach@gmail.com>
2024-07-07 18:20:06 +1000

    Выполнение второго задания

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const http = require("http");
-http.createServer(function(request,response){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    response.end('<h1>Привет, Октагон!</h1>');
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/static', (req, res) => {
+  res.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+});
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end('<h1>Привет, Октагон!</h1>');
+});
+
+app.get('/dynamic', (req, res) => {
+  const { a, b, c } = req.query;
+  const values = [a, b, c].map(Number);
+
+  if (values.some(isNaN)) {
+    res.json({ header: 'Error' });
+  } else {
+    const result = values.reduce((product, value) => product * value, 1) / 3;
+    res.json({ header: 'Calculated', body: result.toString() });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
