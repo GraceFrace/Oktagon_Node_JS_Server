bfdd306d499c385f192e050f253c6d9bcaa7b408
Денис <tolmachevdenistolmach@gmail.com>
2024-07-07 19:38:48 +1000

    Задание 3 (Работа с БД)

--- app.js
+++ app.js
@@ -1,25 +1,116 @@
 const express = require('express');
 const app = express();
+const mysql = require("mysql2");
 const port = 3000;
 
-app.get('/static', (req, res) => {
-  res.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ChatBotTests'
+  });
+
+connection.connect(err=>{
+    if(err){
+        console.error('Error connection to the DB'. err.stack);
+        return;
+    }
+
+    console.log('Connected to the DB')
 });
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end('<h1>Привет, Октагон!</h1>');
+
+
+app.get('/getAllItems', (request, response) =>{
+    connection.query('select * from items', (error, results) => {
+        if(error){
+            response.json({error: 'Error, try again!'});
+        }
+        else{
+            response.json(results);
+        }
+    });
 });
 
-app.get('/dynamic', (req, res) => {
-  const { a, b, c } = req.query;
-  const values = [a, b, c].map(Number);
 
-  if (values.some(isNaN)) {
-    res.json({ header: 'Error' });
+app.post('/addItem', (request, response) => {
+    const {name, description} = request.query;
+    if(!name || !description){
+        response.json(null);
+        return;
+    }
+
+    const query = 'insert into items (name, description) values(?, ?)';
+    connection.query(query, [name, description], (error, results) => {
+        if(error){
+            response.json({error: "Error, try again!!"});
+        }
+        else{
+            response.json({id: results.insertId, name, description});
+        }
+    });
+});
+
+
+app.post('/deleteItem', (request, response) => {
+    const {id} = request.query;
+    if(typeof id === 'undefined' || isNaN(id)){
+        response.json({error: "Ошибка бро"});
+        return;
+    }
+
+    const query = 'delete from items where id = ?';
+    connection.query(query, [id], (error, results) => {
+        if(error){
+            response.json({error: "Error, try again!!"});
+        }
+        else{
+            response.json({id});
+        }
+    });
+});
+
+
+
+app.post('/updateItem', (request, response) => {
+    const {id, name, description} = request.query;
+    if(!id || isNaN(id) || !name || !description){
+        response.json(null);
+        return;
+    }
+
+    const query = 'update items set name = ?, description = ? where id = ?';
+    connection.query(query, [name, description, id], (error, results) => {
+        if(error){
+            response.json({error: "Error, try again!!"});
+        }
+        else{
+            response.json({id, name, description});
+        }
+    });
+});
+
+
+
+app.get('/static', (request, response) => {
+  response.json({ header: 'Hello', body: 'Octagon NodeJS Test' });
+});
+
+app.get('/', (request, response) => {
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.end('<h1>Привет, Октагон!</h1>');
+  });
+
+app.get('/dynamic', (request, response) => {
+  const a = parseFloat(request.query.a);
+  const b = parseFloat(request.query.b);
+  const c = parseFloat(request.query.c);
+
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    response.json({ header: 'Error' });
   } else {
-    const result = values.reduce((product, value) => product * value, 1) / 3;
-    res.json({ header: 'Calculated', body: result.toString() });
+    const result = (a * b * c) / 3;
+    response.json({ header: 'Calculated', body: result.toString() });
   }
 });
 
